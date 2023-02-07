37c742f9ec379fdc7b50b87b7913bb72c72dd103
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-02-08 03:37:11 +0800

    Added routing to main app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages and components
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
