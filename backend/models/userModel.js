05e1be0d461bc1bf3c02039bb745c8d8baf738a7
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-02-08 01:35:40 +0800

    Added server-side validation for signing up

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -16,6 +18,31 @@ const userSchema = new Schema({
 
 // Static signup method
 userSchema.statics.signup = async function (email, password) {
+  // Validation
+  if (!email || !password) {
+    throw Error("All fields are required.");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid.");
+  }
+
+  /**
+   * Defaults to the following parameters:
+   * - Minimum length of 8 characters
+   * - Must have at least:
+   *   - 1 lowercase letter
+   *   - 1 uppercase letter
+   *   - 1 number
+   *   - 1 symbol
+   *
+   * See documentation on https://www.npmjs.com/package/validator
+   * for more details
+   */
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is too weak.");
+  }
+
   // Check if the email is already in use
   const exists = await this.findOne({ email });
   if (exists) {
