5b96eddf1d8f258d9ffeac56eac5711dc8feef43
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-02-08 01:08:48 +0800

    Updated main backend file

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,35 @@
 // Package imports
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Importing routes
+const userRoutes = require("./routes/user");
 
 // Initialize Express app
-const app = express()
+const app = express();
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
+
+// Registering routes
+app.use("/api/user", userRoutes);
 
 // Log requests to console
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // Connect to database
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        // Listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log('Listening on port', process.env.PORT)
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
