5384c9fccce39f16ae885ed2283416c24796cf9d
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-02-08 23:57:39 +0800

    Applied MUI frontend to dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,6 +1,19 @@
 import { useEffect, useState } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { Navigate } from "react-router-dom";
+import {
+  Box,
+  Paper,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Button,
+  Grid,
+} from "@mui/material";
 
 const Dashboard = () => {
   const [users, setUsers] = useState(null);
@@ -30,20 +43,52 @@ const Dashboard = () => {
   }
 
   return (
-    <div className="users">
-      <h2>Users</h2>
-      <ul>
-        {!users ? (
-          <li>No users to display</li>
-        ) : (
-          users.map((user) => (
-            <li>
-              {user.email} | {user.password}
-            </li>
-          ))
-        )}
-      </ul>
-    </div>
+    <Box container sx={{ p: 1, bgcolor: "primary.main", minHeight: "100vh" }}>
+      <Paper sx={{ pl: 5, pb: 5, pr: 5, pt: 2, bgcolor: "#f7f7f7" }}>
+        <Box
+          component="span"
+          m={1}
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="h3">Users</Typography>
+          <Button variant="contained">Log Out</Button>
+        </Box>
+        <TableContainer component={Paper}>
+          <Table aria-label="user table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Email</TableCell>
+                <TableCell>Password</TableCell>
+                <TableCell>ID</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {!users ? (
+                <TableRow
+                  colspan={3}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>No users to display</TableCell>
+                </TableRow>
+              ) : (
+                users.map((user) => (
+                  <TableRow
+                    colspan={3}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell>{user.email}</TableCell>
+                    <TableCell>{user.password}</TableCell>
+                    <TableCell>{user._id}</TableCell>
+                  </TableRow>
+                ))
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </Box>
   );
 };
 
