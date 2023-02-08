a4805ee0a04295177821161e6e509eac9190d2f4
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-02-08 23:21:46 +0800

    Applied MUI frontend for login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,6 +2,15 @@ import { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { Link, Navigate } from "react-router-dom";
+import {
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Paper,
+  Box,
+  Alert,
+} from "@mui/material";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -20,30 +29,80 @@ const Login = () => {
   };
 
   return (
-    <form className="login" onSubmit={handleSubmit}>
-      <h3>Log In</h3>
-
-      <label>Email:</label>
-      <input
-        type="email"
-        onChange={(event) => setEmail(event.target.value)}
-        value={email}
-      />
-
-      <label>Password:</label>
-      <input
-        type="password"
-        onChange={(event) => setPassword(event.target.value)}
-        value={password}
-      />
-
-      <button disabled={isLoading}>Log In</button>
-      <p>
-        Don't have an account? <Link to="/signup">Sign Up</Link>
-      </p>
-      {error && <div>{error}</div>}
-      {message && <div>{message}</div>}
-    </form>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        minHeight: "100vh",
+        bgcolor: "primary.main",
+      }}
+    >
+      <Grid item xs={3}>
+        <Paper variant="outlined" sx={{ p: 1 }}>
+          <div className="login-wrapper">
+            <Typography variant="h4" sx={{ mt: 2, ml: 1, mr: 1 }}>
+              Log In
+            </Typography>
+            <form className="signup" onSubmit={handleSubmit}>
+              <TextField
+                type="email"
+                required
+                label="Email"
+                onChange={(event) => setEmail(event.target.value)}
+                sx={{ mt: 1, ml: 1, mr: 1 }}
+                value={email}
+              />
+              <br />
+              <TextField
+                type="password"
+                required
+                label="Password"
+                onChange={(event) => setPassword(event.target.value)}
+                sx={{ mt: 1, ml: 1, mr: 1 }}
+                value={password}
+              />
+              {error && (
+                <Alert severity="error" sx={{ mt: 1, ml: 1, mr: 1 }}>
+                  {error}
+                </Alert>
+              )}
+              {message && (
+                <Alert severity="success" sx={{ mt: 1, ml: 1, mr: 1 }}>
+                  {message}
+                </Alert>
+              )}
+              <Box
+                display="flex"
+                justifyContent="flex-end"
+                alignItems="flex-end"
+              >
+                <Button
+                  disabled={isLoading}
+                  type="submit"
+                  variant="contained"
+                  sx={{ mr: 1, mb: 1, mt: 1 }}
+                >
+                  Sign Up
+                </Button>
+              </Box>
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                sx={{ mt: 1, mb: 1 }}
+              >
+                <Typography>
+                  Don't have an account? <Link to="/signup">Sign Up</Link>
+                </Typography>
+              </Box>
+            </form>
+          </div>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
